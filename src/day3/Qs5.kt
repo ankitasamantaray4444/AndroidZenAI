b6cf16548049a776d6fe65a7db4ea1a124bea6c3
ankitasamantaray4444 <ankitasamantaray0@gmail.com>
2025-06-06 20:18:17 +0530

    complted

---
+++ src/day3/Qs5.kt
@@ -0,0 +1,12 @@
+package day3
+
+const val orderno = 5
+fun main(){
+
+    if (orderno % 2 == 0){
+        println("orderno : $orderno is even (yum!)")
+    }else{
+        println("orderno : $orderno is odd (tasty!)")
+
+    }
+}
