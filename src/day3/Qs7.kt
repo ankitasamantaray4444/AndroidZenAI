0f69caf8c5e9e01249d0996f0e5e8cd804d656dd
ankitasamantaray4444 <ankitasamantaray0@gmail.com>
2025-06-06 21:05:29 +0530

    complted

---
+++ src/day3/Qs7.kt
@@ -0,0 +1,12 @@
+package day3
+
+fun main() {
+    var coffeprice = 1
+
+    while (coffeprice <= 10) {
+        if (coffeprice % 2 == 0) {
+            println("coffeprice : $coffeprice")
+        }
+           coffeprice++
+    }
+}
