9196b12c75485f3446a43d0d9f1e19e93fba7c56
ankitasamantaray4444 <ankitasamantaray0@gmail.com>
2025-06-06 21:26:19 +0530

    complted

---
+++ src/day3/Qs9.kt
@@ -0,0 +1,10 @@
+package day3
+
+fun main() {
+    val age = 63
+    if (age >= 60) {
+        println("You are eligible for a senior discount.")
+    } else {
+        println("You are not eligible for a senior discount.")
+    }
+}
