9d7258e0ddcb1cfde6f7bcb716660213e05058a6
ankitasamantaray4444 <ankitasamantaray0@gmail.com>
2025-06-06 21:44:16 +0530

    complted

---
+++ src/day3/Qs10.kt
@@ -0,0 +1,12 @@
+package day3
+
+fun main(){
+ val caffeine = 75
+    val strength = when {
+        caffeine <= 30 ->"mild"
+                caffeine <= 75 ->"medium"
+            caffeine <= 100 ->"strong"
+        else -> "invalid caffeine level"
+    }
+      println("coffe strength is $strength")
+}
