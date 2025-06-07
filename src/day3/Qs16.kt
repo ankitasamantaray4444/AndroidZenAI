940659f7f7ecc4fe40a4406037d8d3049e569bb1
ankitasamantaray4444 <ankitasamantaray0@gmail.com>
2025-06-07 15:21:24 +0530

    complted

---
+++ src/day3/Qs16.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val orderName = "Latte"
+
+    if (orderName.length >= 5) {
+        println("Your order name '$orderName' has enough characters.")
+    } else {
+        println("The order name '$orderName' is too short.")
+    }
+}
